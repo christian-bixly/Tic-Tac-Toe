c16c96f349bba98ebb53b440b902d8dffe450c3c
Christian Sta. Ana <christian@bixly.com>
2011-01-28 20:16:28 +0800

    - Game playable to completion vs AI who takes first available move

--- cmgtest/tictactoe/urls.py
+++ cmgtest/tictactoe/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('tictactoe.views',
     (r'^$', 'index'),
+    url(r'^ai_move$', 'ai_move', name='ai_move'),
 )
--- cmgtest/tictactoe/views.py
+++ cmgtest/tictactoe/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
+import json
 
 def index(request):
     return render_to_response('index.html', {
@@ -6,6 +8,14 @@ def index(request):
         'cols': range(3)
     })
 
+def ai_move(request):
+    board = json.loads(request.POST.get('board'))
+    
+    for i, row in enumerate(board):
+        for j, cell in enumerate(row):
+            if cell is None:
+                return HttpResponse(json.dumps({'row': i, 'col': j}))
+    
 # TODO:
 # First make game playable, with random AI
 # Generate moves DB
